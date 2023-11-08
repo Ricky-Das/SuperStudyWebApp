9613e71925eb97947c8521da3cc7300e82c6a815
Ricky Das <lightning-ricky10@hotmail.com>
2023-11-08 11:23:02 -0600

    Working on NavBar component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  purge: { "./src/**/*.{js,ts,jsx,tsx,mdx}": [] }, // './src/**/*.{js,ts,jsx,tsx,mdx}
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      defaultColors: {
+        accent1: "#7DB54C",
+        accent2: "#344727",
+        "primary-bg-color": "#FFFFFF",
+        grey: "#808080",
+      },
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+        serif: ["DM Serif Display", "serif"],
       },
     },
   },
   plugins: [],
-}
+};
