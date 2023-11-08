b1434de2fa4dd4b16eb7fcbe26cb539a2c8f903c
Ricky Das <lightning-ricky10@hotmail.com>
2023-11-08 16:08:26 -0600

    Fixing git issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   mode: "jit",
   purge: { "./src/**/*.{js,ts,jsx,tsx,mdx}": [] }, // './src/**/*.{js,ts,jsx,tsx,mdx}
   content: [
@@ -18,8 +19,25 @@ module.exports = {
       fontFamily: {
         sans: ["DM Sans", "sans-serif"],
         serif: ["DM Serif Display", "serif"],
+=======
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+>>>>>>> 9da698d (Initial commit from Create Next App)
       },
     },
   },
   plugins: [],
+<<<<<<< HEAD
 };
+=======
+}
+>>>>>>> 9da698d (Initial commit from Create Next App)
